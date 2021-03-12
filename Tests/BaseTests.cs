cb9bcd82503ce9d4e9d1db2f54bc763a05cf3d24
Victor Tereschenko <sibvic@gmail.com>
2021-03-12 19:37:11 +0600

    test: Base indicator

---
+++ Tests/BaseTests.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProfitRobots.TradeScriptConverter.Generators.TestsCases;
+
+namespace ProfitRobots.TradeScriptConverter.Generators.MQL5.Tests
+{
+    [TestClass]
+    public class BaseTests
+    {
+        [TestMethod]
+        public void IndicatorHeader()
+        {
+            var script = Base.CreateIndicatorEmptyScript();
+            string code = IndicatorGenerator.Generate(script);
+            throw new System.NotImplementedException();
+        }
+    }
+}
