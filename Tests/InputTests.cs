cf768a37fa9de434b5780469ee3faee104dacdd9
Victor Tereschenko <sibvic@gmail.com>
2021-03-23 21:01:09 +0600

    test: Source input

--- Tests/InputTests.cs
+++ Tests/InputTests.cs
@@ -73,7 +73,7 @@ void OnInit()
         }
 
         [TestMethod]
-        public void String()
+        public void StringInput()
         {
             var script = Input.CreateStringParameter();
             string code = IndicatorGenerator.Generate(script);
@@ -116,6 +116,38 @@ void OnInit()
 #property indicator_plots 0
 
 input bool a = " + ((script.Parameters[0] as BoolParameter).DefaultValue ? "true" : "false") + "; // " + script.Parameters[0].Title + @"
+input int bars_limit = 1000; // Bars limit
+
+" + Constants.NAME_PREFIX + @"
+void OnInit()
+{
+   IndicatorObjPrefix = GenerateIndicatorPrefix(""HGVSAVA"");
+   IndicatorSetString(INDICATOR_SHORTNAME, """ + ModelGenerator.DefaultIndicatorTitle + @""");
+   IndicatorSetInteger(INDICATOR_DIGITS, Digits());
+}
+
+" + Constants.EMPTY_DEINIT + @"
+" + Constants.ONCALC_HEADER + "\r\n" + Constants.PRE_FOR + @"
+   for (int pos = MathMax(rates_total - 1 - bars_limit, MathMax(first, prev_calculated - 1)); pos < rates_total; ++pos)
+   {
+      int oldPos = rates_total - pos - 1;
+   }
+" + Constants.POST_FOR + @"}
+");
+        }
+
+        [TestMethod]
+        public void SourceInput()
+        {
+            var script = Input.CreateSourceParameter();
+            string code = IndicatorGenerator.Generate(script);
+            Verifier.CompareLineByLine(code, @"#property strict
+
+#property indicator_chart_window
+#property indicator_buffers 0
+#property indicator_plots 0
+
+input ENUM_APPLIED_PRICE a = PRICE_CLOSE; // " + script.Parameters[0].Title + @"
 input int bars_limit = 1000; // Bars limit
 
 " + Constants.NAME_PREFIX + @"
