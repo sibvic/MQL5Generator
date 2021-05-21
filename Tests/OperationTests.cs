5317f7f48f2eb170e6a824198b8ec853c6f50912
Victor Tereschenko <sibvic@gmail.com>
2021-05-21 18:36:52 +0600

    test: Add IfWithReturnValue dummy

---
+++ Tests/OperationTests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProfitRobots.TradeScriptConverter.Common;
+using ProfitRobots.TradeScriptConverter.Generators.TestsCases;
+
+namespace ProfitRobots.TradeScriptConverter.Generators.MQL5.Tests
+{
+    [TestClass]
+    public class OperationTests
+    {
+        [TestMethod]
+        public void IfWithReturnValue()
+        {
+            var script = Operations.CreateIfWithReturnValue();
+            string code = IndicatorGenerator.Generate(script);
+            var reference = System.IO.File.ReadAllText(System.IO.Path.Combine("ref", "OperationTests", "IfWithReturnValue.mq5"));
+            Verifier.CompareLineByLine(code, reference);
+        }
+    }
+}
