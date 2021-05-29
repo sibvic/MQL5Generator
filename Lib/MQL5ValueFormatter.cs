4a6d553e84c70e22a942756c4048cd849bf11b3e
Victor Teresxhenko <sibvic@gmail.com>
2021-05-29 10:09:33 +0600

    reafactor: Use base class for MQL4/5

--- Lib/MQL5ValueFormatter.cs
+++ Lib/MQL5ValueFormatter.cs
@@ -1,35 +1,8 @@
 ﻿using ProfitRobots.TradeScriptConverter.Generators.MQLBase;
-using ProfitRobots.TradeScriptConverter.Model;
 
 namespace ProfitRobots.TradeScriptConverter.Generators.MQL5
 {
-    class MQL5ValueFormatter : IValueFormatter
+    class MQL5ValueFormatter : AValueFormatter
     {
-        public string Format(Value value)
-        {
-            switch (value.Type)
-            {
-                case Model.ValueType.Operation:
-                    return FormatOperation(value);
-                case Model.ValueType.Constant:
-                case Model.ValueType.Variable:
-                    return value.Content;
-                default:
-                    break;
-            }
-            return "";
-        }
-
-        #region Operations
-        private string FormatOperation(Value variable)
-        {
-            switch (variable.Id)
-            {
-                case "=":
-                    return Format(variable.Parameters[0]) + " = " + Format(variable.Parameters[1]) + ";";
-            }
-            return "";
-        }
-        #endregion
     }
 }
