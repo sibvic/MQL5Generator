93aac742ed2bb6544a51cf52885fcd64649fc070
Victor Teresxhenko <sibvic@gmail.com>
2021-06-18 18:53:10 +0600

    test: Add triary operator

--- Tests/OperationTests.cs
+++ Tests/OperationTests.cs
@@ -51,5 +51,14 @@ namespace ProfitRobots.TradeScriptConverter.Generators.MQL5.Tests
             var reference = System.IO.File.ReadAllText(System.IO.Path.Combine("ref", "OperationTests", "Equal.mq5"));
             Verifier.CompareLineByLine(code, reference);
         }
+
+        [TestMethod]
+        public void Triary()
+        {
+            var script = Operations.CreateTriary();
+            string code = IndicatorGenerator.Generate(script);
+            var reference = System.IO.File.ReadAllText(System.IO.Path.Combine("ref", "OperationTests", "Triary.mq5"));
+            Verifier.CompareLineByLine(code, reference);
+        }
     }
 }
