844a8d7dfdfae21933a0ed4c64aa3e93abf5281e
Victor Tereschenko <sibvic@gmail.com>
2021-06-30 21:01:07 +0600

    test: Add assign color test

---
+++ Tests/ConstantsTests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProfitRobots.TradeScriptConverter.Common;
+using ProfitRobots.TradeScriptConverter.Generators.TestsCases;
+
+namespace ProfitRobots.TradeScriptConverter.Generators.MQL5.Tests
+{
+    [TestClass]
+    public class ConstantsTests
+    {
+        [TestMethod]
+        public void AssignColor()
+        {
+            var script = Constants.CreateAssignColor();
+            string code = IndicatorGenerator.Generate(script);
+            var reference = System.IO.File.ReadAllText(System.IO.Path.Combine("ref", "ConstantsTests", "AssignColor.mq5"));
+            Verifier.CompareLineByLine(code, reference);
+        }
+    }
+}
