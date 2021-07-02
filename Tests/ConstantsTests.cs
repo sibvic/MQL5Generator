f97e33afc909861df5164aff11e5e0c9ab08fd35
Victor Tereschenko <sibvic@gmail.com>
2021-07-02 20:31:02 +0600

    test: Support bool variables

--- Tests/ConstantsTests.cs
+++ Tests/ConstantsTests.cs
@@ -24,5 +24,14 @@ namespace ProfitRobots.TradeScriptConverter.Generators.MQL5.Tests
             var reference = System.IO.File.ReadAllText(System.IO.Path.Combine("ref", "ConstantsTests", "AssignFloat.mq5"));
             Verifier.CompareLineByLine(code, reference);
         }
+
+        [TestMethod]
+        public void AssignBoolean()
+        {
+            var script = Constants.CreateAssignBoolean();
+            string code = IndicatorGenerator.Generate(script);
+            var reference = System.IO.File.ReadAllText(System.IO.Path.Combine("ref", "ConstantsTests", "AssignBoolean.mq5"));
+            Verifier.CompareLineByLine(code, reference);
+        }
     }
 }
