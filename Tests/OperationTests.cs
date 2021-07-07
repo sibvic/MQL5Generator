ef4e21772570f142db89fbf024a40efa80d925f8
Victor Tereschenko <sibvic@gmail.com>
2021-07-07 20:12:05 +0600

    test: Support group

--- Tests/OperationTests.cs
+++ Tests/OperationTests.cs
@@ -123,5 +123,14 @@ namespace ProfitRobots.TradeScriptConverter.Generators.MQL5.Tests
             var reference = System.IO.File.ReadAllText(System.IO.Path.Combine("ref", "OperationTests", "And.mq5"));
             Verifier.CompareLineByLine(code, reference);
         }
+
+        [TestMethod]
+        public void Group()
+        {
+            var script = Operations.CreateGroup();
+            string code = IndicatorGenerator.Generate(script);
+            var reference = System.IO.File.ReadAllText(System.IO.Path.Combine("ref", "OperationTests", "Group.mq5"));
+            Verifier.CompareLineByLine(code, reference);
+        }
     }
 }
