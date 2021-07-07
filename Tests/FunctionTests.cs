f0b518eba2efda8cafbf8310687e94dc8eb09e1c
Victor Tereschenko <sibvic@gmail.com>
2021-07-07 18:12:32 +0600

    test: Support lowest function

--- Tests/FunctionTests.cs
+++ Tests/FunctionTests.cs
@@ -35,6 +35,15 @@ namespace ProfitRobots.TradeScriptConverter.Generators.MQL5.Tests
         }
 
         [TestMethod]
+        public void Lowest()
+        {
+            var script = Functions.CreateLowest();
+            var code = IndicatorGenerator.Generate(script);
+            var reference = System.IO.File.ReadAllText(System.IO.Path.Combine("ref", "FunctionTests", "Lowest.mq5"));
+            Verifier.CompareLineByLine(code, reference);
+        }
+
+        [TestMethod]
         public void Wma()
         {
             var script = Functions.CreateWma();
