594c857e051a4bda2acb5f6f5894920468f49d80
Victor Tereschenko <sibvic@gmail.com>
2021-07-07 20:05:26 +0600

    test: Support string values

--- Tests/ConstantsTests.cs
+++ Tests/ConstantsTests.cs
@@ -42,5 +42,14 @@ namespace ProfitRobots.TradeScriptConverter.Generators.MQL5.Tests
             var reference = System.IO.File.ReadAllText(System.IO.Path.Combine("ref", "ConstantsTests", "AssignNA.mq5"));
             Verifier.CompareLineByLine(code, reference);
         }
+
+        [TestMethod]
+        public void AssignString()
+        {
+            var script = Constants.CreateAssignString();
+            string code = IndicatorGenerator.Generate(script);
+            var reference = System.IO.File.ReadAllText(System.IO.Path.Combine("ref", "ConstantsTests", "AssignString.mq5"));
+            Verifier.CompareLineByLine(code, reference);
+        }
     }
 }
